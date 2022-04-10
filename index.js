d7f73619f1e9d65fc2189bf28eb80da56226f8e6
Hosanna Bivan <hosabivan@gmail.com>
2022-04-10 20:32:56 +0100

    commit

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+let output = document.getElementById("output");
 
+let buttons = Array.from(document.querySelectorAll("button"));
+
+buttons.map( button => {
+    button.addEventListener("click", (e) => {
+        switch(e.target.innerText){
+            case 'RESET':
+                output.innerText = "";
+                break;
+            case 'DEL':
+                if (output.innerText){
+                    output.innerText = output.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try{
+                    output.innerText = eval(output.innerText);
+                }
+                catch{
+                    output.innerText = 'Error';
+                }
+                break;
+            default:
+                output.innerText += e.target.innerText;
+        }
+    });
+});
 
 
  
